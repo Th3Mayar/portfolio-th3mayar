4057ed437993adf4316b958f09537ffcb4abcc9c
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-05-23 11:17:26 -0400

    feat: 🚀 PT-1 push app portfolio-th3mayar

---
+++ src/i18n/ui.ts
@@ -0,0 +1,14 @@
+import en from '@/lang/en.json';
+import es from '@/lang/es.json';
+
+export const languages = {
+    en: 'English',
+    es: 'Español',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+    en,
+    es,
+} as const;
---
+++ src/i18n/utils.ts
@@ -0,0 +1,18 @@
+import { ui, defaultLang } from './ui';
+
+export function getLangFromUrl(url: URL) {
+    const [, lang] = url.pathname.split('/');
+    if (lang in ui) return lang as keyof typeof ui;
+    return defaultLang;
+}
+
+export function useTranslations(lang: keyof typeof ui) {
+    return ui[lang] || ui[defaultLang];
+}
+
+export function useTranslatedPath(lang: keyof typeof ui) {
+    return function translatePath(path: string | URL, l: string = lang) {
+        if (path instanceof URL) path = path.pathname;
+        return `/${l}${path}`;
+    };
+}
