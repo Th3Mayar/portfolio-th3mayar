4effdc3eccff54823949cb241dedcf15548bd87d
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-05-24 14:23:19 -0400

    feat: PT-1 add and config language

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,14 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [vue(), tailwind()],
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+    routing: {
+        prefixDefaultLocale: true,
+        redirectToDefaultLocale: true,
+        strategy: 'pathname',
+    },
+},
 });
