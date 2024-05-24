3e578bd64ed120c1fb611268171b47f7e59e8ba2
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-05-24 14:22:11 -0400

    feat: PT-1 add route for ez change image

---
+++ public/routeImg.ts
@@ -0,0 +1,21 @@
+export const routes = {
+  projects: {
+    codesys: "public/assets/codesys.PNG",
+    priceNotify: "public/assets/price-notify.PNG"
+  },
+  logos: {
+    css: "public/logos/css.svg",
+    html: "public/logos/html.svg",
+    js: "public/logos/js.svg",
+    react: "public/logos/react.svg",
+    node: "public/logos/nodejs.svg",
+    express: "public/logos/express.svg",
+    mongo: "public/logos/mongodb.svg",
+    jquery: "public/logos/jquery.svg",
+    tailwind: "public/logos/tailwindcss.svg",
+    php: "public/logos/php.svg",
+    vue: "public/logos/vue.svg",
+    next: "public/logos/nextjs.svg",
+    astro: "public/favicon.svg"
+  }
+};
