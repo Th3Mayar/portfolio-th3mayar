94c2ff119af38c7d6f03b4848dc5a29c254fef92
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-05-26 00:27:04 -0400

    fix: add correct sintaxys and path name

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     routing: {
       prefixDefaultLocale: true,
       redirectToDefaultLocale: true,
-      // strategy: 'pathname',
+      strategy: 'pathname',
     },
   },
   vite: {
