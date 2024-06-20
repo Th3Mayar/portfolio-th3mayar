989a91c6e5d49424abc0927b082f801ec1984548
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-06-20 00:35:22 -0400

    feat: add dropdown menu and others features

--- src/components/atoms/Toggle.vue
+++ src/components/atoms/Toggle.vue
@@ -11,14 +11,8 @@
     <button class="menu-toggle" @click="toggleMenu" v-if="isMenuOpen">
       <IconByName name="X" color="simple" className="header-icon" />
     </button>
-    
-    <ul v-if="isMenuOpen" class="items">
-      <Item href="" title={TComponents.name} class="item-name link-card" />
-      <Item href="" title={TComponents.hello} class="item-hello link-card item" />
-      <Item href="" title={TComponents.aboutMe} class="item-about link-card item" />
-      <Item href="" title={TComponents.projects} class="item-projects link-card item" />
-      <li class="item-hidden"></li>
-      <Item href="" title={TComponents.contactMe} class="item-contact link-card item" />
+    <ul v-if="isMenuOpen" class="items-dropdown">
+     <slot />
     </ul>
   </div>
 </template>
@@ -43,3 +37,104 @@ export default {
   },
 };
 </script>
+
+<style>
+.items-dropdown {
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 70px;
+  right: 25px;
+  align-items: left;
+  gap: 20px;
+  padding: 1rem 1rem 0px 1rem;
+  width: 92%;
+}
+
+.items-dropdown li {
+  display: flex;
+  list-style-type: none;
+  border: none;
+  align-items: center;
+  border-bottom: 1px solid var(--border);
+  padding: 20px 0;
+}
+
+.custom-item {
+}
+@media (max-width: 926px) {
+  .items-dropdown {
+    width: 94.7%;
+  }
+}
+@media (max-width: 896) {
+  .items-dropdown {
+    width: 94.6%;
+  }
+}
+@media (max-width: 894px) {
+  .items-dropdown {
+    width: 94.5%;
+  }
+}
+@media (max-width: 826px) {
+  .items-dropdown {
+    width: 93.9%;
+  }
+}
+@media (max-width: 760px) {
+  .items-dropdown {
+    width: 93.5%;
+  }
+}
+@media (max-width: 666px) {
+  .items-dropdown {
+    width: 92.8%;
+  }
+}
+@media (max-width: 642px) {
+  .items-dropdown {
+    width: 92.6%;
+  }
+}
+@media (max-width: 590px) {
+  .items-dropdown {
+    width: 91.8%;
+  }
+}
+@media (max-width: 532px) {
+  .items-dropdown {
+    width: 91%;
+  }
+}
+@media (max-width: 464px) {
+  .items-dropdown {
+    width: 89.6%;
+  }
+}
+@media (max-width: 416px) {
+  .items-dropdown {
+    width: 88.5%;
+  }
+}
+@media (max-width: 376px) {
+  .items-dropdown {
+    width: 87.5%;
+  }
+}
+@media (max-width: 344px) {
+  .items-dropdown {
+    width: 86%;
+  }
+}
+@media (max-width: 310px) {
+  .items-dropdown {
+    width: 85%;
+  }
+}
+@media (max-width: 290px) {
+  .items-dropdown {
+    width: 83%;
+  }
+}
+</style>
