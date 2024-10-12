b155817696bc4c5bfe4941ea974e6b6a65f0a290
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-10-12 09:14:39 -0400

    ci: fix error from up to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
-  adapter: vercel(),
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'en',
