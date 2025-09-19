f78f5fade2dec28c117f3f4ddcf29673cc8b9465
Th3Mayar <josehenriquez.02.26@gmail.com>
2025-09-19 12:54:46 -0400

    chore: :zap: remove mt-20 from the MobileOverlay

--- src/components/molecule/MobileOverlay.vue
+++ src/components/molecule/MobileOverlay.vue
@@ -8,7 +8,6 @@
           viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          class="mt-20"
         >
           <path
             d="M12 2L2 7L12 12L22 7L12 2Z"
