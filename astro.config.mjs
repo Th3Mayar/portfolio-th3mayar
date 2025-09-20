926ea5974040a1fb018056d20ee34771d3a55913
Th3Mayar <josehenriquez.02.26@gmail.com>
2025-09-19 22:42:29 -0400

    fix: :bug: fix routing to en or es on vercel, fix vercel json and astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
+  output: "server",
+  adapter: vercel(),
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'en',
